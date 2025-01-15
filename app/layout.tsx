b3eff5ecdb132b5dedc43807a9cdc3e27440e363
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-01-15 10:08:29 +0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { ReactNode } from "react";
+
+import "./globals.css";
+
+const ibmPlexSans = localFont({
+  src: [
+    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
+  ],
+});
+
+const bebasNeue = localFont({
+  src: [
+    { path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
+  ],
+  variable: "--bebas-neue",
+});
+
+export const metadata: Metadata = {
+  title: "BookWise",
+  description:
+    "BookWise is a book lending university library management solution. It allows users to easily find and borrow books, track their loan status, and manage their account information.",
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <html lang="en">
+      <body
+        className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
