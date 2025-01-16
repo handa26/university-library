76e6f88d11217a8b826bf821b73aabfd4fff06c3
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-01-16 10:29:09 +0800

    feat: add BookCard component and integrate it into BookList; update styles for book button

---
+++ components/BookCard.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import BookCover from "@/components/BookCover";
+import { Button } from "@/components/ui/button";
+
+import { cn } from "@/lib/utils";
+
+const BookCard = ({
+  id,
+  title,
+  genre,
+  coverColor,
+  coverUrl,
+  isLoanedBook = false,
+}: Book) => {
+  return (
+    <li className={cn(isLoanedBook && "w-full xs:w-52")}>
+      <Link
+        href={`/books/${id}`}
+        className={cn(isLoanedBook && "w-full flex flex-col items-center")}
+      >
+        <BookCover coverColor={coverColor} coverImage={coverUrl} />
+
+        <div className={cn("mt-4", !isLoanedBook && "max-w-28 xs:max-w-40")}>
+          <div className="book-title">{title}</div>
+          <div className="book-genre">{genre}</div>
+        </div>
+
+        {isLoanedBook && (
+          <div className="mt-3 w-full">
+            <div className="book-loaned">
+              <Image
+                src="/icons/calendar.svg"
+                alt="calendar"
+                width={18}
+                height={18}
+                className="object-contain"
+              />
+              <p className="text-light-100">11 days left to return</p>
+            </div>
+
+            <Button className="book-btn">Download receipt</Button>
+          </div>
+        )}
+      </Link>
+    </li>
+  );
+};
+
+export default BookCard;
