3ccc0fb7123dafa47d9eae1c86b46bab149c2e26
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-01-23 09:35:17 +0800

    feat: track user activity by updating lastActivityDate in the database

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,36 @@
 import { ReactNode } from "react";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
+import { eq } from "drizzle-orm";
 
 import Header from "@/components/Header";
 
 import { auth } from "@/auth";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
   
   if (!session) redirect("/sign-in");
+
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    // get user and see if they have been active today
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session.user.id))
+      .limit(1);
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10)) return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session.user.id));
+  });
 
   return (
     <main className="root-container">
