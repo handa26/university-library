2800ec68e6788324191f409ce37af195a6965611
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-01-28 16:35:09 +0800

    feat: implement book creation functionality with success and error notifications

--- components/admin/forms/BookForm.tsx
+++ components/admin/forms/BookForm.tsx
@@ -20,6 +20,8 @@ import FileUpload from "@/components/FileUpload";
 import ColorPicker from "@/components/admin/ColorPicker";
 
 import { bookSchema } from "@/lib/validations";
+import { createBook } from "@/lib/admin/actions/book";
+import { toast } from "@/hooks/use-toast";
 
 interface Props extends Partial<Book> {
   type?: "CREATE" | "UPDATE";
@@ -45,7 +47,22 @@ const BookForm = ({ type, ...book }: Props) => {
   });
 
   const onSubmit = async (values: z.infer<typeof bookSchema>) => {
-    console.log(values);
+    const result = await createBook(values);
+
+    if (result.success) {
+      toast({
+        title: "Success",
+        description: "Book added successfully",
+      });
+
+      router.push(`/admin/books/${result.data.id}`);
+    } else {
+      toast({
+        title: "Error adding book",
+        description: result.message,
+        variant: "destructive"
+      });
+    }
   };
 
   return (
