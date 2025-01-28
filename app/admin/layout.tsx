421938856bbe8d4fcb8d508ffe3275ba04de4cad
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-01-28 16:42:01 +0800

    feat: add admin role check in layout to restrict access for non-admin users

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,16 +1,28 @@
 import { ReactNode } from "react";
 import { redirect } from "next/navigation";
+import { eq } from "drizzle-orm";
 
 import Sidebar from "@/components/admin/Sidebar";
 import Header from "@/components/admin/Header";
 
 import { auth } from "@/auth";
 import "@/styles/admin.css";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
 
   if (!session?.user?.id) redirect("/sign-in");
+
+  const isAdmin = await db
+    .select({ isAdmin: users.role})
+    .from(users)
+    .where(eq(users.id, session.user.id))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === "ADMIN");
+
+  if (!isAdmin) redirect("/");
 
   return (
     <main className="flex min-h-screen w-full flex-row">
