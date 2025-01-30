6cfcedcbb774ce2f144c968281c7d7ee2aa90bf8
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-01-30 10:25:50 +0800

    feat: add database seeding script and update package.json with seed command

--- constants/index.ts
+++ constants/index.ts
@@ -55,6 +55,61 @@ export const FIELD_TYPES = {
   password: "password",
 };
 
+export const sorts = [
+  {
+    value: "oldest",
+    label: "Oldest",
+  },
+  {
+    value: "newest",
+    label: "Newest",
+  },
+  {
+    value: "available",
+    label: "Available",
+  },
+  {
+    value: "highestRated",
+    label: "Highest Rated",
+  },
+];
+
+export const userRoles = [
+  {
+    value: "user",
+    label: "User",
+    bgColor: "bg-[#FDF2FA]",
+    textColor: "text-[#C11574]",
+  },
+  {
+    value: "admin",
+    label: "Admin",
+    bgColor: "bg-[#ECFDF3]",
+    textColor: "text-[#027A48]",
+  },
+];
+
+export const borrowStatuses = [
+  {
+    value: "overdue",
+    label: "Overdue",
+    bgColor: "bg-[#FFF1F3]",
+    textColor: "text-[#C01048]",
+  },
+  {
+    value: "borrowed",
+    label: "Borrowed",
+    bgColor: "bg-[#F9F5FF]",
+    textColor: "text-[#6941C6]",
+  },
+  {
+    value: "returned",
+    label: "Returned",
+    bgColor: "bg-[#F0F9FF]",
+    textColor: "text-[#026AA2]",
+  },
+];
+
 export const sampleBooks = [
   {
     id: 1,
@@ -191,57 +246,3 @@ export const sampleBooks = [
   },
 ];
 
-export const sorts = [
-  {
-    value: "oldest",
-    label: "Oldest",
-  },
-  {
-    value: "newest",
-    label: "Newest",
-  },
-  {
-    value: "available",
-    label: "Available",
-  },
-  {
-    value: "highestRated",
-    label: "Highest Rated",
-  },
-];
-
-export const userRoles = [
-  {
-    value: "user",
-    label: "User",
-    bgColor: "bg-[#FDF2FA]",
-    textColor: "text-[#C11574]",
-  },
-  {
-    value: "admin",
-    label: "Admin",
-    bgColor: "bg-[#ECFDF3]",
-    textColor: "text-[#027A48]",
-  },
-];
-
-export const borrowStatuses = [
-  {
-    value: "overdue",
-    label: "Overdue",
-    bgColor: "bg-[#FFF1F3]",
-    textColor: "text-[#C01048]",
-  },
-  {
-    value: "borrowed",
-    label: "Borrowed",
-    bgColor: "bg-[#F9F5FF]",
-    textColor: "text-[#6941C6]",
-  },
-  {
-    value: "returned",
-    label: "Returned",
-    bgColor: "bg-[#F0F9FF]",
-    textColor: "text-[#026AA2]",
-  },
-];
