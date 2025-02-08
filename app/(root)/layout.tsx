e4d257981d8fc14f99607901a22328d2e47ea323
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-02-08 10:55:21 +0800

    feat: make layout container full width for improved responsiveness

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -34,7 +34,7 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
   return (
     <main className="root-container">
-      <div className="mx-auto max-w-7xl">
+      <div className="mx-auto max-w-7xl w-full">
         <Header session={session} />
         <div className="mt-20 pb-20">{children}</div>
       </div>
